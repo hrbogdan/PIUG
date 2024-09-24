e1241813f26228c93075905ab79aaf8688444f88
Hari <raicubogdan811@gmail.com>
2024-09-24 15:19:31 +0300

    Login form/page

---
+++ haribo-frontend/src/app/components/partials/input-validation/input-validation.component.ts
@@ -0,0 +1,50 @@
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+
+const VALIDATORS_MESSAGES: any = {
+  required: 'This field should not be empty',
+  email: 'Please enter a valid email address',
+};
+
+@Component({
+  selector: 'input-validation',
+  templateUrl: './input-validation.component.html',
+  styleUrls: ['./input-validation.component.css'],
+})
+export class InputValidationComponent implements OnInit, OnChanges {
+  @Input()
+  control!: AbstractControl;
+  @Input()
+  showErrorsWhen = true;
+  errorMessages: string[] = [];
+
+  ngOnInit(): void {
+    this.control.statusChanges.subscribe(() => {
+      this.checkValidation();
+    });
+    this.control.valueChanges.subscribe(() => {
+      this.checkValidation();
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.checkValidation();
+  }
+
+  checkValidation() {
+    const errors = this.control.errors;
+    if (!errors) {
+      this.errorMessages = [];
+      return;
+    }
+
+    const errorKeys = Object.keys(errors);
+    this.errorMessages = errorKeys.map((key) => VALIDATORS_MESSAGES[key]);
+  }
+}
