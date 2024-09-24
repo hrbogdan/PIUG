087858ec85eca30d66b0fd9a1f619d7ccac26626
Hari <raicubogdan811@gmail.com>
2024-09-24 07:20:56 +0300

    login-logout functionalities

--- haribo-frontend/src/app/services/user.service.ts
+++ haribo-frontend/src/app/services/user.service.ts
@@ -4,23 +4,52 @@ import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { IUserLogin } from '../shared/Interfaces/IUserLogin';
 import { HttpClient } from '@angular/common/http';
 import { USER_LOGIN_URL } from '../shared/constants/urls';
+import { ToastrService } from 'ngx-toastr';
+
+const USER_KEY = 'user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private userSubject = new BehaviorSubject<User>(new User());
+  private userSubject = new BehaviorSubject<User>(
+    this.getUserFromLocalStorage()
+  );
   public userObservable: Observable<User>;
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private toastr: ToastrService) {
     this.userObservable = this.userSubject.asObservable();
   }
 
   login(userLogin: IUserLogin): Observable<User> {
     return this.http.post<User>(USER_LOGIN_URL, userLogin).pipe(
       tap({
-        next: (user) => {},
-        error: (errorResponse) => {},
+        next: (user) => {
+          this.setUserToLocalStorage(user);
+          this.userSubject.next(user);
+          this.toastr.success(`Login successful ${user.name}`);
+        },
+        error: (errorResponse) => {
+          this.toastr.error(errorResponse.error, 'Login failed');
+        },
       })
     );
+  }
+
+  logout() {
+    this.userSubject.next(new User());
+    localStorage.removeItem(USER_KEY);
+    window.location.reload();
+  }
+
+  private setUserToLocalStorage(user: User) {
+    localStorage.setItem(USER_KEY, JSON.stringify(user));
+  }
+
+  private getUserFromLocalStorage(): User {
+    const userJson = localStorage.getItem(USER_KEY);
+    if (userJson) {
+      return JSON.parse(userJson) as User;
+    }
+    return new User();
   }
 }
